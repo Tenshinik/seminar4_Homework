05afaf5048bf16d0af5d95ca833069834da2ebf9
Tenshinik <@tenshinik@gmail.com>
2022-12-14 12:08:12 +0800

    examples examp2 project

---
+++ Examples/examp2/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+Строки использовать нельззя
+*/
+
+Console.WriteLine("Enter number");
+int h = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+while (h > 0)
+{
+int num = h % 10;
+h = h / 10;
+sum = sum + num;
+}
+Console.WriteLine("the sum of all the digits in the number is: " + sum);
